c793d96537a90af8542f737e691b7cb47ec71a3d
namanswamy <namanswami3696@gmail.com>
2024-08-07 01:13:48 +0530

    commit2

--- app/src/main/java/com/example/contactsapp/MainActivity.java
+++ app/src/main/java/com/example/contactsapp/MainActivity.java
@@ -7,9 +7,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.contactsapp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ContactDatabase contactDatabase;
+    private ArrayList<Contacts> contacts = new ArrayList<>();
+
+    private MyAdapter myAdapter;
+
+    private ActivityMainBinding mainBinding;
+    private MainActivityClickHandlers handlers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +34,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        handlers = new MainActivityClickHandlers(this);
+
+        mainBinding.setClickHandler(handlers);
+
+        RecyclerView recyclerView = mainBinding.recyclerview;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
     }
 }
