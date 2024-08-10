ef6b303dd2afa76a9e5c2dbce7c6263683cb9461
namanswamy <namanswami3696@gmail.com>
2024-08-10 16:58:22 +0530

    commit3

--- app/src/main/java/com/example/contactsapp/AddNewContactActivity.java
+++ app/src/main/java/com/example/contactsapp/AddNewContactActivity.java
@@ -6,13 +6,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.contactsapp.databinding.ActivityAddNewContactBinding;
+import com.example.contactsapp.databinding.ActivityMainBinding;
 
 public class AddNewContactActivity extends AppCompatActivity {
 
+    private ActivityAddNewContactBinding binding;
+    private AddNewContactClickHandler handler;
+    private Contacts contacts;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_add_new_contact);
+
+        contacts = new Contacts();
+
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_add_new_contact
+        );
+
+        MyViewModel myViewModel = new ViewModelProvider(this)
+                .get(MyViewModel.class);
+
+        handler = new AddNewContactClickHandler(
+                contacts,
+                this,
+                myViewModel
+        );
+
+        binding.setContact(contacts);
+        binding.setClickHandler(handler);
     }
 }
--- app/src/main/java/com/example/contactsapp/AddNewContactClickHandler.java
+++ app/src/main/java/com/example/contactsapp/AddNewContactClickHandler.java
@@ -5,13 +5,18 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
 public class AddNewContactClickHandler {
     Contacts contact;
     Context context;
+    MyViewModel myViewModel;
 
-    public AddNewContactClickHandler(Contacts contact, Context context) {
+    public AddNewContactClickHandler(Contacts contact, Context context , MyViewModel myViewModel) {
         this.contact = contact;
         this.context = context;
+        this.myViewModel = myViewModel;
     }
 
     public void onSubmitBtnClicked(View view){
@@ -20,8 +25,13 @@ public class AddNewContactClickHandler {
         }
         else{
             Intent i = new Intent(context, MainActivity.class);
-            i.putExtra("Name",contact.getName());
-            i.putExtra("Name",contact.getEmail());
+//            i.putExtra("Name",contact.getName());
+//            i.putExtra("Name",contact.getEmail());
+            Contacts c = new Contacts(
+                    contact.getName(),
+                    contact.getEmail()
+            );
+            myViewModel.addNewContact(c);
             context.startActivity(i);
         }
     }
