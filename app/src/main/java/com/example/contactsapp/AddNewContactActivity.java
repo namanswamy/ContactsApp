6b2f7284bf369157ab92336eb9c0d13a0bb546d7
namanswamy <namanswami3696@gmail.com>
2024-08-11 11:31:07 +0530

    commit 7

--- app/src/main/java/com/example/contactsapp/AddNewContactActivity.java
+++ app/src/main/java/com/example/contactsapp/AddNewContactActivity.java
@@ -1,37 +1,40 @@
 package com.example.contactsapp;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import com.example.contactsapp.databinding.ActivityAddNewContactBinding;
-import com.example.contactsapp.databinding.ActivityMainBinding;
 
 public class AddNewContactActivity extends AppCompatActivity {
 
     private ActivityAddNewContactBinding binding;
     private AddNewContactClickHandler handler;
     private Contacts contacts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_add_new_contact);
 
         contacts = new Contacts();
 
-        binding = DataBindingUtil.setContentView(
+        binding= DataBindingUtil.setContentView(
                 this,
                 R.layout.activity_add_new_contact
         );
 
+
+        // VIew Model
         MyViewModel myViewModel = new ViewModelProvider(this)
                 .get(MyViewModel.class);
+
 
         handler = new AddNewContactClickHandler(
                 contacts,
@@ -41,5 +44,15 @@ public class AddNewContactActivity extends AppCompatActivity {
 
         binding.setContact(contacts);
         binding.setClickHandler(handler);
+
+
+
+
+
+
+
+
     }
+
+
 }
--- app/src/main/java/com/example/contactsapp/AddNewContactClickHandler.java
+++ app/src/main/java/com/example/contactsapp/AddNewContactClickHandler.java
@@ -5,34 +5,40 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
 
-public class AddNewContactClickHandler {
+public class AddNewContactClickHandler  {
+
     Contacts contact;
     Context context;
     MyViewModel myViewModel;
 
-    public AddNewContactClickHandler(Contacts contact, Context context , MyViewModel myViewModel) {
+    public AddNewContactClickHandler(Contacts contact,
+                                     Context context,
+                                     MyViewModel myViewModel) {
         this.contact = contact;
         this.context = context;
         this.myViewModel = myViewModel;
+
     }
 
     public void onSubmitBtnClicked(View view){
-        if(contact.getName() == null || contact.getEmail() == null){
+        if (contact.getName() == null || contact.getEmail() ==null){
             Toast.makeText(context, "Fields Cannot be empty", Toast.LENGTH_SHORT).show();
         }
         else{
-            Intent i = new Intent(context, MainActivity.class);
-//            i.putExtra("Name",contact.getName());
-//            i.putExtra("Name",contact.getEmail());
+            Intent i = new Intent(context,MainActivity.class);
+
             Contacts c = new Contacts(
                     contact.getName(),
                     contact.getEmail()
             );
+
             myViewModel.addNewContact(c);
+
             context.startActivity(i);
+
         }
     }
 }
